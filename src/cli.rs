028c1b32a584e399f42b2cf979d241296542027c
George Merebashvili <abracadabragio12@gmail.com>
2025-10-02 23:53:30 +0400

    Update cli name

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
-#[command(name = "AppInstaller")]
+#[command(name = "AppHatch")]
 #[command(version = "0.1.0")]
 #[command(author = "CCXLV")]
 #[command(about = "Installs AppImages easily")]
